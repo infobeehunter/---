7ae8911b5fec1d47c20d7527c5981994e0668585
infobeehunter <info.beehunter@gmail.com>
2026-03-15 22:14:26 +0100

    Create icon-symbol.tsx

---
+++ components/icon-symbol.tsx
@@ -0,0 +1,14 @@
+import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
+import { Pressable } from "react-native";
+import * as Haptics from "expo-haptics";
+export function HapticTab(props: BottomTabBarButtonProps) {
+ return (
+ <Pressable
+ {...props}
+ onPress={(e) => {
+ Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+ props.onPress?.(e);
+ }}
+ />
+ );
+}
