29ccf52d091175035bc2945992d2de0d7c21dd1d
infobeehunter <info.beehunter@gmail.com>
2026-03-16 18:15:36 +0100

    Delete hooks directory

--- hooks/use-colors.ts
+++
@@ -1,7 +0,0 @@
-import { Colors, type ColorScheme, type ThemeColorPalette } from "@/constants/t
-import { useColorScheme } from "./use-color-scheme";
-export function useColors(colorSchemeOverride?: ColorScheme): ThemeColorPalette
- const colorSchema = useColorScheme();
- const scheme = (colorSchemeOverride ?? colorSchema ?? "light") as ColorScheme
- return Colors[scheme];
-}
